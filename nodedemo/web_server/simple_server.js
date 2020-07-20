c43862b1ed0f1e05cfc50549d63cf5e262e94fc6
suzl <joexsue@gmail.com>
2020-07-20 23:40:27 +0800

    :speech_balloon: delete
    delete

--- nodedemo/web_server/simple_server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http')
-http
-	.createServer(function(request, response) {
-		response.writeHead(200, { 'Content-Type': 'text/plain' })
-		response.end('Hello node.js')
-	})
-	.listen(8080)
-
-console.log('$ Server is running at http://localhost:8080')
